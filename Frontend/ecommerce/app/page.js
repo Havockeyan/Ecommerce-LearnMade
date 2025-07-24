6566dd49ecc31770adefe52424d15fc326f29244
SAIRAMSSSS <hashiramasenju6.2006@gmail.com>
2025-07-24 08:23:33 +0530

    feat: completed footer

--- Frontend/ecommerce/app/page.js
+++ Frontend/ecommerce/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
     <Banner />
-    
+    <Footer />
     </main>
   );
 }
