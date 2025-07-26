adea58a1935b215d6443e3839be653397adda46b
ragulkannan08 <rahul08supermacy@gmail.com>
2025-07-26 22:23:29 +0530

    added searchbar but in console error occures

--- Frontend/ecommerce/app/page.js
+++ Frontend/ecommerce/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
+import SearchBar from "./components/SearchBar";
 
 export default function Home() {
   return (
     <main>
+    <SearchBar />
     <Banner />
     
     </main>
