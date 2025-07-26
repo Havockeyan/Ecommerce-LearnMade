8a160ee06f2750b616f5531d787a48eb637ded17
LakshmikanthanC <geelakshmi2003@gmail.com>
2025-07-26 20:36:35 +0530

    Login

--- Frontend/ecommerce/app/page.js
+++ Frontend/ecommerce/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Signup from "./components/Login/Signup";
+import Login from "./components/Login/Login";
 
 export default function Home() {
   return (
     <main>
+    <Signup />
+    <Login/>
     <Banner />
     <Footer />
     </main>
